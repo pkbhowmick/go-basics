2c18fb0d7c7afb7ff0a4fa4d2d9586cdb293b4a4
pkbhowmick <pkbhowmick007@gmail.com>
2020-11-25 23:21:55 +0600

    C6:master Complete REST API server with CRUD operation without DB

--- Web/server.go
+++ Web/server.go
@@ -6,11 +6,12 @@ import (
 	"log"
 	"encoding/json"
 	"math/rand"
+	"strconv"
 )
 
 	
 type Book struct {
-	ID     int    `json:"id"`
+	ID     string `json:"id"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
 	ISBN   string `json:"ISBN"`
@@ -28,15 +29,68 @@ func createBook(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type" , "application/json")
 	var book Book
 	json.NewDecoder(r.Body).Decode(&book)
-	book.ID = rand.Intn(100000)
+	book.ID = strconv.Itoa(rand.Intn(100000))
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
 }
+
+func getBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type" , "application/json")
+	param := mux.Vars(r)
+	var book Book
+	for index, item := range books {
+		if param["id"] == item.ID {
+			book = books[index]
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message":"Book not exist!"}`))
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type" , "application/json")
+	param := mux.Vars(r)
+	var newBook Book
+	json.NewDecoder(r.Body).Decode(&newBook)
+	for index, item := range books {
+		if param["id"] == item.ID {
+			newBook.ID = item.ID
+			books = append(books[:index],books[index+1:]...)
+			books = append(books,newBook)
+			json.NewEncoder(w).Encode(newBook)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message":"Book not exist!"}`))
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type" , "application/json")
+	param := mux.Vars(r)
+	var book Book
+	for index, item := range books {
+		if param["id"] == item.ID {
+			book = books[index]
+			books = append(books[:index],books[index+1:]...)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message":"Book not exist!"}`))
+}
+
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/books",getBooks).Methods("GET")
 	router.HandleFunc("/api/books",createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}",getBook).Methods("GET")
+	router.HandleFunc("/api/books/{id}",updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}",deleteBook).Methods("DELETE")
 	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
